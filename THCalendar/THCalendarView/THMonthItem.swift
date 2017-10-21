8541307f3d13cb5dce189b1b8f0d581617fbe5bf
thierryH91200 <thierry.hentic@gmail.com>
2017-10-21 11:13:16 +0200

    init

--- THCalendar/THCalendarView/THMonthItem.swift
+++ THCalendar/THCalendarView/THMonthItem.swift
@@ -11,38 +11,35 @@ import Cocoa
 class THMonthItem: NSCollectionViewItem {
 
     @IBOutlet weak var monthField: NSTextField!
+
+    var attributesMonth = [NSAttributedStringKey: AnyObject]()
+    var attributesYear = [NSAttributedStringKey: AnyObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
-    }
-    
-    func configure(month: String, year: Int) {
-        let month = String(month.prefix(1)).uppercased() + String(month.dropFirst())
         
         let paragraphStyle = NSParagraphStyle.default.mutableCopy() as! NSMutableParagraphStyle
         paragraphStyle.lineBreakMode = .byTruncatingTail
         paragraphStyle.alignment = .center
-        
-        var attributesMonth = [NSAttributedStringKey: AnyObject]()
+
         attributesMonth[.foregroundColor] = NSColor.white
         attributesMonth[.font] = NSFont(name: "Helvetica", size: 20.0)
         attributesMonth[.paragraphStyle] = paragraphStyle
         
-        let title = NSMutableAttributedString(string: month, attributes: attributesMonth )
-        
-        var attributesYear = [NSAttributedStringKey: AnyObject]()
         attributesYear[.foregroundColor] = NSColor.red
         attributesYear[.font] = NSFont(name: "Helvetica", size: 20.0)
         attributesYear[.paragraphStyle] = paragraphStyle
-
+    }
+    
+    func configure(month: String, year: Int) {
+        
+        let month = String(month.prefix(1)).uppercased() + String(month.dropFirst())
+        let title = NSMutableAttributedString(string: month, attributes: attributesMonth )
         let yearTitle = NSAttributedString(string: "  \(year)", attributes : attributesYear )
 
         title.append(yearTitle)
         
         monthField.attributedStringValue = title
     }
-    
-
-    
 }
